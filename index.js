f9441b5a6868ea0a0c4f264108caa590f3a5a0fe
Aurora Padilla <123429652+auro-fre34@users.noreply.github.com>
2025-03-21 12:11:38 -0600

    landing completo

--- index.js
+++ index.js
@@ -34,3 +34,35 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     }
 });
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const menuToggle = document.querySelector(".menu-toggle");
+    const navLinks = document.querySelector(".nav-links");
+
+    menuToggle.addEventListener("click", function () {
+        navLinks.classList.toggle("active");
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    const scrollBtn = document.getElementById("scrollToTopBtn");
+
+    // Mostrar el botón cuando se baja 100px
+    window.addEventListener("scroll", () => {
+        if (window.scrollY > 100) {
+            scrollBtn.style.display = "block";
+        } else {
+            scrollBtn.style.display = "none";
+        }
+    });
+
+    // Volver al inicio al hacer clic
+    scrollBtn.addEventListener("click", () => {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
+    });
+});
+
